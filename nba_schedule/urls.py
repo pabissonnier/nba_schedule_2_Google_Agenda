28251a4b2a4aab59ee8fe66df109fcf97c08a5fa
pabissonnier <pabissonnier@gmail.com>
2020-01-05 17:50:19 +0100

    Index inserted

--- nba_schedule/urls.py
+++ nba_schedule/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls import include, url
+from mainapp import views as mainapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^$', mainapp_views.index, name='home'),
 ]
 
 
