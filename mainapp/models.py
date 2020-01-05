792f05fa757e74a907127783ac10742b92fa9de0
pabissonnier <pabissonnier@gmail.com>
2020-01-05 19:40:38 +0100

    some changes

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+from difflib import SequenceMatcher
+
+
+class Schedule(models.Model):
+    date = models.CharField(max_length=25, null=True)
+    hour = models.CharField(max_length=25, null=True)
+    vteam = models.CharField(max_length=25, null=True)
+    hteam = models.CharField(max_length=25, null=True)
+    ingredients = models.CharField(max_length=5000, null=True)
+    shops = models.CharField(max_length=1000, null=True)
+    link = models.CharField(max_length=1000, null=True, unique=True)
+    picture = models.URLField()
+    favorites = models.ManyToManyField(User, related_name='favorite', blank=True)
+    labels = models.CharField(max_length=1000, null=True)
