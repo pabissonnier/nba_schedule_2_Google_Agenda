f92fba78c7046e4e92429fd61b94c68ad2071da8
pabissonnier <pabissonnier@gmail.com>
2020-01-25 13:14:56 +0100

    Adding script to generate the popin calendar confirmation

---
+++ mainapp/test_calendar.py
@@ -0,0 +1,13 @@
+event = {
+  'summary': 'Google I/O 2015',
+  'location': '800 Howard St., San Francisco, CA 94103',
+  'description': 'A chance to hear more about Google\'s developer products.',
+  'start': {
+    'dateTime': '2020-01-25T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'end': {
+    'dateTime': '2015-05-28T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+}
