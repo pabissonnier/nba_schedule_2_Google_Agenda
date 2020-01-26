5fd44f998e1c942d05289753e0c318fc63796d16
pabissonnier <pabissonnier@gmail.com>
2020-01-26 17:54:32 +0100

    Trying script to transform js variable to python

--- mainapp/js_to_python.py
+++ mainapp/js_to_python.py
@@ -1,2 +1,17 @@
-import js2py
+"""import js2py
 
+context = js2py.EvalJs({'python_sum': sum})
+js_code = static/mainapp/js/googleapi.js
+context.execute(js_code)
+print(context.profile)"""
+
+import re
+
+js = open("static/mainapp/js/googleapi.js", "r").readlines()
+
+matcher_rex = re.compile(r'^var\s+(?P<varname>\w+)\s+=\s+"(?P<varvalue>[\w\s]+)";?$')
+for line in js:
+    matches = matcher_rex.match(line)
+    if matches:
+        name, value = matches.groups()
+        print (name, value)
