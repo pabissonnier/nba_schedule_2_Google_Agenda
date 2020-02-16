1c837b6d92776f509c2b052eb39d070492319385
pabissonnier <pabissonnier@gmail.com>
2020-02-16 23:49:58 +0100

    Testing combination of caldnar creation plu sevent insertion on upload button

--- mainapp/calendar_insertion.py
+++ mainapp/calendar_insertion.py
@@ -3,6 +3,12 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 from datetime import datetime
 
+game_list = [{'summary': 'Indiana Pacers @ Denver Nuggets', 'location': 'Stadium', 'description': 'Your schedule from NS2GC', 'start': {'dateTime': '2020-01-19T20:00:00', 'timeZone': 'America/New_York'}, 'end': {'dateTime': '2020-01-19T23:00:00', 'timeZone': 'America/New_York'}},
+{'summary': 'Houston Rockets @ Denver Nuggets', 'location': 'Stadium', 'description': 'Your schedule from NS2GC', 'start': {'dateTime': '2020-01-26T15:30:00', 'timeZone': 'America/New_York'}, 'end': {'dateTime': '2020-01-26T18:30:00', 'timeZone': 'America/New_York'}},
+{'summary': 'Utah Jazz @ Denver Nuggets', 'location': 'Stadium', 'description': 'Your schedule from NS2GC', 'start': {'dateTime': '2020-01-30T22:30:00', 'timeZone': 'America/New_York'}, 'end': {'dateTime': '2020-01-30T01:30:00', 'timeZone': 'America/New_York'}},
+{'summary': 'Portland Trail Blazers @ Denver Nuggets', 'location': 'Stadium', 'description': 'Your schedule from NS2GC', 'start': {'dateTime': '2020-02-04T22:00:00', 'timeZone': 'America/New_York'}, 'end': {'dateTime': '2020-02-04T01:00:00', 'timeZone': 'America/New_York'}},
+{'summary': 'San Antonio Spurs @ Denver Nuggets', 'location': 'Stadium', 'description': 'Your schedule from NS2GC', 'start': {'dateTime': '2020-02-10T21:00:00', 'timeZone': 'America/New_York'}, 'end': {'dateTime': '2020-02-10T00:00:00', 'timeZone': 'America/New_York'}},
+{'summary': 'Los Angeles Lakers @ Denver Nuggets', 'location': 'Stadium', 'description': 'Your schedule from NS2GC', 'start': {'dateTime': '2020-02-12T22:00:00', 'timeZone': 'America/New_York'}, 'end': {'dateTime': '2020-02-12T01:00:00', 'timeZone': 'America/New_York'}}]
 
 def calendar_connection():
     SCOPES = "https://www.googleapis.com/auth/calendar"
@@ -18,32 +24,23 @@ def calendar_connection():
 def calendar_insertion(service):
     calendar = {
         'summary': 'Your NBA team(s) Schedule',
-        'timeZone': 'America/Los_Angeles'
+        'timeZone': 'America/New_York'
     }
 
     created_calendar = service.calendars().insert(body=calendar).execute()
 
-    print(created_calendar['id'])
+    calendar_id = created_calendar['id']
+    return calendar_id
 
 
-def event_insertion(service, event):
-    calendar_list_entry = service.calendarList().get(calendarId='primary').execute()
+def event_insertion(service, calendar_id, event):
+    calendar_list_entry = service.calendarList().get(calendarId=calendar_id).execute()
     if calendar_list_entry['accessRole']:
-        event = {
-            'summary': "test",
-            'location': 'gg',
-            'description': 'gg',
-            'start': {
-                'dateTime': '2020-02-18T09:00:00-07:00',
-                'timeZone': 'America/New_York',
-            },
-            'end': {
-                'dateTime': '2020-02-19T09:00:00-07:00',
-                'timeZone': 'America/New_York',
-            },
-        }
-
-        event = service.events().insert(calendarId='primary', body=event).execute()
+        event = service.events().insert(calendarId=calendar_id, body=event).execute()
         print(f"The event has been created! View it at {event.get('htmlLink')}!")
 
-calendar_insertion(calendar_connection())
+
+service = calendar_connection()
+calendar_id = calendar_insertion(service)
+for game in game_list:
+    event_insertion(service, calendar_id, game)
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import users_data
 from .models import Schedule
+from .calendar_insertion import calendar_connection, calendar_insertion, event_insertion
 
 
 def index(request):
@@ -10,31 +11,22 @@ def index(request):
 
 def upload_page(request):
     schedule = Schedule()
-    global no_teams
-    no_teams = False
     teams_list = request.GET.getlist('team')
     schedule_list = Schedule.get_teams_agenda(schedule, teams_list)
-    if len(schedule_list) > 0:
-        no_teams = False
-        games_list = []
-        for schedule in schedule_list:
-            for game in schedule:
-                game_dict = Schedule.extraction_to_gformat(schedule, game, teams_list)
-                games_list.append(game_dict)
-            context = {
-                'teams': teams_list,
-                'schedule': schedule_list,
-                'noteams': no_teams,
-                'games': games_list,
-            }
-            return render(request, 'mainapp/upload.html', context)
-    else:
-        no_teams = True
-        message = "Pick one or several teams"
-        context = {
-            'teams': teams_list,
-            'schedule': schedule_list,
-            'noteams': no_teams,
-            'message': message,
-        }
-        return render(request, 'mainapp/upload.html', context)
+    games_list = []
+    for schedule in schedule_list:
+        for game in schedule:
+            game_dict = Schedule.extraction_to_gformat(schedule, game, teams_list)
+            games_list.append(game_dict)
+            """service = calendar_connection()
+            calendar_insertion(service)
+            for game in games_list:
+                event_insertion(service, game)"""
+
+    context = {
+        'teams': teams_list,
+        'schedule': schedule_list,
+        'games': games_list,
+    }
+    return render(request, 'mainapp/upload.html', context)
+
