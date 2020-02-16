0e596176e6fa61daf6f02e29ddcee72900847e4a
pabissonnier <pabissonnier@gmail.com>
2020-02-16 18:00:20 +0100

    Event insertion now working

--- mainapp/calendar_insertion.py
+++ mainapp/calendar_insertion.py
@@ -1,87 +1,35 @@
-from __future__ import print_function
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-import pickle
-import datetime
+from datetime import datetime
 
 
+def calendar_connection():
+    SCOPES = "https://www.googleapis.com/auth/calendar"
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('calendar', 'v3', http=creds.authorize(Http()))
 
-def calendar_creation():
-    SCOPES = ['https://www.googleapis.com/auth/calendar']
-    calendar = {
-    'summary': 'calendarSummary',
-    'timeZone': 'America/Los_Angeles'
-    }
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+    calendar_list_entry = service.calendarList().get(calendarId='primary').execute()
+    if calendar_list_entry['accessRole']:
+        event = {
+            'summary': "test",
+            'location': 'gg',
+            'description': 'gg',
+            'start': {
+                'dateTime': '2020-02-18T09:00:00-07:00',
+                'timeZone': 'America/New_York',
+            },
+            'end': {
+                'dateTime': '2020-02-19T09:00:00-07:00',
+                'timeZone': 'America/New_York',
+            },
+        }
 
-    service = build('calendar', 'v3', credentials=creds)
-    created_calendar = service.calendars().insert(body=calendar).execute()
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        print(f"The event has been created! View it at {event.get('htmlLink')}!")
 
-    print (created_calendar['id'])
-
-calendar_creation()
-"""
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-
-
-def main():
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                          maxResults=10, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-
-
-if __name__ == '__main__':
-    main()"""
+calendar_connection()
