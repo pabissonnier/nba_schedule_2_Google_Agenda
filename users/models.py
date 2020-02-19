50ad99fd82e245b01e0db658480ff939da3f8f72
Pierre-Antoine Bissonnier <pabissonnier@gmail.com>
2020-02-19 08:04:18 -0800

    Adding team and player models to user app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=250)
+    stadium = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=250)
+    team = models.OneToOneField(
+        Team,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    def __str__(self):
+        return self.name
