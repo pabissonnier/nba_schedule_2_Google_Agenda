c24ce955bde7bdc002f3f400ae625d329dac0b2e
Pierre-Antoine Bissonnier <pabissonnier@gmail.com>
2020-02-21 08:40:05 -0800

    When user chose products, they get to favs

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
 from .models import Schedule
+from users.models import Team
 from .calendar_insertion import calendar_connection, calendar_insertion, event_insertion
 
 
@@ -13,21 +16,25 @@ def upload_page(request):
     teams_list = request.GET.getlist('team')
     schedule_list = Schedule.get_teams_agenda(schedule, teams_list)
     games_list = []
-    service = calendar_connection()
+    for team in teams_list:
+        team_to_insert = get_object_or_404(Team, name=team)
+        team_to_insert.favorite.add(request.user)
+    """service = calendar_connection()
     calendar_id = calendar_insertion(service)
     for schedule in schedule_list:
         for game in schedule:
             game_dict = Schedule.extraction_to_gformat(schedule, game, teams_list)
             games_list.append(game_dict)
             for game in games_list:
-                event_insertion(service, calendar_id, game)
+                event_insertion(service, calendar_id, game)"""
 
     context = {
         'teams': teams_list,
-        'schedule': len(schedule_list),
-        'games': len(games_list),
+        #'schedule': schedule_list,
+        #'games': games_list,
         #"service": service,
         #'calendar_id': calendar_id,
     }
     return render(request, 'mainapp/upload.html', context)
+
 
