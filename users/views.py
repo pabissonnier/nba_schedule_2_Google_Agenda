bfd84b9b92401c48183c5f01641d8d7359422a89
Pierre-Antoine Bissonnier <pabissonnier@gmail.com>
2020-02-21 08:35:09 -0800

    New function for favs and product details in users views

--- users/views.py
+++ users/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+
+from .models import Team
 
 
 @login_required()
 def profile(request):
     return render(request, 'users/profile.html')
+
+
+""""@login_required()
+def favs(request):
+    team = get_object_or_404(Team, id=request.POST.get('fav-btn'))
+    team.favorite.add(request.user)
+    return redirect(request.META['HTTP_REFERER']"""
+
+
+@login_required()
+def show_favs(request):
+    teams_list = Team.objects.filter(favorite=request.user).order_by('name')
+    title = "Your favorite team(s)"
+    context = {
+        'teams_list': teams_list,
+        'title': title,
+    }
+    return render(request, 'users/favs.html', context)
+
+@login_required()
+def teams_detail(request, team_name):
+    team = get_object_or_404(Team, name=team_name)
+    context = {
+        'title': team.name,
+        'picture': team.picture,
+        'conference': team.conference,
+        'division': team.division
+    }
+    return render(request, 'users/detail.html', context)
