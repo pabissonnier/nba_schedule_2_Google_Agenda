70cf17af2e49099c59d9f7be8e82ac65d628a63d
Pierre-Antoine Bissonnier <pabissonnier@gmail.com>
2020-02-26 01:23:03 -0800

    Adding function to get infos from json games API

--- users/team_datas.py
+++ users/team_datas.py
@@ -20,3 +20,20 @@ class TeamInfos:
 
         return json_loads
 
+    def get_infos_from_json(self, json_loads):
+        """ Get infos from games API """
+        nba_game_list = []
+        for game in json_loads["api"]["games"]:
+            game_list = []
+            date = game["startTimeUTC"][:10]
+            hteam = game["hTeam"]["fullName"]
+            vteam = game["vTeam"]["fullName"]
+            hteam_score = game["hTeam"]["score"]["points"]
+            vteam_score = game["vTeam"]["score"]["points"]
+            game_list.append(date)
+            game_list.append(hteam)
+            game_list.append(hteam_score)
+            game_list.append(vteam)
+            game_list.append(vteam_score)
+            nba_game_list.append(game_list)
+        return nba_game_list
