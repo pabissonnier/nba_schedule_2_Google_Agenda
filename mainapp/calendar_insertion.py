e7a86d5c39231602532adf63f29675aa0344a894
pabissonnier <pabissonnier@gmail.com>
2020-03-04 22:53:30 +0100

    Some changes on the navbar

--- mainapp/calendar_insertion.py
+++ mainapp/calendar_insertion.py
@@ -12,26 +12,14 @@ import os.path
 def calendar_connection():
     SCOPES = "https://www.googleapis.com/auth/calendar"
 
-    """store = file.Storage('mainapp/token.pickle')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('mainapp/credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('calendar', 'v3', http=creds.authorize(Http()))
-    return service"""
-
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+
     if os.path.exists('mainapp/token.pickle'):
         with open('mainapp/token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         flow = InstalledAppFlow.from_client_secrets_file('mainapp/credentials.json', SCOPES)
         creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
         with open('mainapp/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
