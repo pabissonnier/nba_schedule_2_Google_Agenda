bb82f305e83009974d803cb0e1bd98f073d64787
pabissonnier <pabissonnier@gmail.com>
2020-03-04 21:44:14 +0100

    Event bug now correctedé

--- mainapp/views.py
+++ mainapp/views.py
@@ -27,32 +27,14 @@ def upload_page(request):
         team_to_insert.favorite.add(request.user)
 
     schedule_list = Schedule.get_teams_agenda(schedule, teams_list)
-    #games_list = []
-
     for schedule_detail in schedule_list:
         for game in schedule_detail:
             game_dict = Schedule.extraction_to_gformat(schedule, game)
-            event_summary = game_dict["summary"]
-            event_start = game_dict['start']['dateTime']
-            event_list = service.events().list(calendarId=calendar_id).execute()
-            for event_list_entry in event_list['items']:
-                if event_list_entry['summary'] == event_summary and event_list_entry['start']['dateTime'] == event_start:
-                    event_exist = True
-                else:
-                    event_exist = False
-                if not event_exist:
-                    event_insertion(service, calendar_id, game_dict)
-                else:
-                    pass
-            #games_list.append(game_dict)
-            #for event in games_list:
-
-
+            event_insertion(service, calendar_id, game_dict)
 
     context = {
         'teams': len(teams_list),
         "service": service,
-        "schedule_list": schedule_list
     }
     return render(request, 'mainapp/upload.html', context)
 
