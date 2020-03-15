ca0763176393ce1c15da1d3b1905dfa746be915d
pabissonnier <pabissonnier@gmail.com>
2020-03-15 11:39:23 +0100

    Trying to modify token access to only get authorization page and not login

--- mainapp/calendar_insertion.py
+++ mainapp/calendar_insertion.py
@@ -6,17 +6,21 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 from datetime import datetime
 from google_auth_oauthlib.flow import InstalledAppFlow
+import oauth2client
 import allauth
 from allauth.socialaccount.models import SocialToken
+from oauth2client import file, client
 import google.auth.credentials
 import pickle
 import os.path
 
 
-def calendar_connection(request, user):
-    SCOPES = "https://www.googleapis.com/auth/calendar"
-    flow = InstalledAppFlow.from_client_secrets_file('mainapp/credentials.json', SCOPES)
-    creds = flow.credentials(allauth.socialaccount.models.SocialToken()
+def calendar_connection():
+    SCOPES = ["https://www.googleapis.com/auth/calendar"]
+    flow = InstalledAppFlow.from_client_secrets_file('mainapp/credentials.json', scopes=SCOPES)
+    creds = flow.authorized_session()
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+    creds = client.AccessTokenCredentials(SocialToken.token, user_agent)
     service = build('calendar', 'v3', credentials=creds)
     return service
 
