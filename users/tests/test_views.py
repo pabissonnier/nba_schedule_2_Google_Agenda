3a04ad5fc2b51bc59aae74cd5f2ad0b4a29fcbb1
pabissonnier <pabissonnier@gmail.com>
2020-03-20 15:41:33 +0100

    Users views tests ok

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -7,14 +7,20 @@ class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_index_page(self):
-        response = self.client.get(reverse('home'))
-
+    def test_profile_page(self):
+        response = self.client.get(reverse('profile'), follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'mainapp/index.html')
 
-    def test_upload_page(self):
-        response = self.client.get(reverse('upload'))
-
+    def test_show_favs_page(self):
+        response = self.client.get(reverse('show_favs'), follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'mainapp/upload.html')
+
+    def test_contact_page(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/contact.html')
+
+    def test_mentions_page(self):
+        response = self.client.get(reverse('mentions'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/mentions.html')
